f5fab69bb3fd07216694e21c7c38650673b449f1
kudaT <timilehinkudaisi01@gmail.com>
2025-03-11 09:41:04 -0700

    feat(navbar): Styled navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
  /** @type {import('tailwindcss').Config} */
  export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#003366",
+        customGray1: "#3D3D3D"
+      }
+    },
   },
   plugins: [],
 }
