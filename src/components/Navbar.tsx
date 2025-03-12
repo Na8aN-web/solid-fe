65b42cfba45e1472079dd060abfed80de72a758f
kudaT <timilehinkudaisi01@gmail.com>
2025-03-12 05:52:08 -0700

    Styled navbar hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,12 +16,12 @@ import steering from "../images/steering.svg";
 import filters from "../images/filter.svg";
 import coolingHeat from "../images/cooling-heat.svg";
 import airCondition from "../images/air-conditioner.svg";
-
+import carHeader from "../images/car-header.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <header>
+    <div className="flex flex-col">
+      <header className="border-b">
         <div className="flex justify-between items-center py-11 px-5 border-b">
           <div className="flex gap-20">
             <img src={solidLogo} alt="solid-logo" />
@@ -53,6 +53,7 @@ const Navbar = () => {
               </button>
             </div>
           </div>
+          {/* User Options */}
           <div className="flex gap-4 items-center">
             <div className="flex gap-1">
               <img src={help} alt="" className="w-8" />
@@ -82,15 +83,21 @@ const Navbar = () => {
         {/* Main Navigation with Sidebar Dropdown */}
         <div>
           <nav>
-            <div className="flex items-start w-full gap-10 pt-6 pr-20">
+            <div className="flex items-start w-full gap-10 pt-6 pb-4 pr-20">
               {/* Sidebar Contents */}
-              <aside>
-                <div className="h-screen">
+              <aside className="relative">
+                <div className="">
                   <div className="flex justify-around items-center w-72 h-14 bg-primary">
-                    <p className="text-base text-white font-normal">All Categories</p>
-                    <img src={arrowDownWhite} alt="arrow-down" className="w-3" />
+                    <p className="text-base text-white font-normal">
+                      All Categories
+                    </p>
+                    <img
+                      src={arrowDownWhite}
+                      alt="arrow-down"
+                      className="w-3"
+                    />
                   </div>
-                  <ul className="flex flex-col justify-around items-start gap h-3/4 px-11 py-3 bg-white text-gray-500 text-sm font-normal">
+                  {/* <ul className="flex flex-col justify-around items-start h-inherit px-11 py-3 bg-white text-gray-500 text-sm font-normal">
                     <li className="flex items-center gap-4">
                       <img src={bodyParts} alt="body-part" />
                       <span>Body Parts</span>
@@ -132,20 +139,23 @@ const Navbar = () => {
                       <span>Air Conditioning</span>
                     </li>
                     <li>See More +</li>
-                  </ul>
+                  </ul> */}
                 </div>
               </aside>
               {/* main navigation */}
               <ul className="flex w-full justify-between items-center pt-1 text-customGray1 text-sm font-semibold">
-                <li className="bg-gray-100 rounded-3xl px-6 py-3">All Brands</li>
+                <li className="bg-gray-100 rounded-3xl px-6 py-3">
+                  All Brands
+                </li>
                 <li>Toyota</li>
-                <li>Merceded</li>
+                <li>Mercedes</li>
                 <li>BMW</li>
                 <li>Ford</li>
                 <li>Honda</li>
                 <li>Hyundai</li>
                 <li>Kia</li>
                 <li>Mazda</li>
+                <li>Audi</li>
                 <li>
                   <img src={arrowRight} alt="arrow-right" className="w-4" />
                 </li>
@@ -153,6 +163,43 @@ const Navbar = () => {
             </div>
           </nav>
         </div>
+        {/* Hero content */}
+        <section className="bg-white pr-8">
+          <div className="flex justify-between w-full items-center bg-customLight pt-0">
+            <div className="bg-white flex items-center justify-between w-full h-3/4 pl-14 ml-0 overflow-hidden">
+              <div>
+                <p className="text-sm text-customBrown font-normal pb-6">
+                  WELCOME TO SOLID SPARE PARTS
+                </p>
+                <h1 className="text-4xl text-customBrown font-semibold w-80 leading-tight pb-8">
+                  Quality Auto Spare Parts at a go!
+                </h1>
+                <button className="border h-12 w-28 rounded-lg bg-primary text-white text-sm font-semibold">
+                  Shop Now
+                </button>
+              </div>
+              <div className="flex-shrink-0">
+                <img src={carHeader} alt="" className="w-[580px]" />
+              </div>
+            </div>
+            <div className="flex flex-col justify-between gap-6 pl-5">
+              <div className="bg-white flex flex-col items-center justify-center w-[300px] h-64">
+                <p className="text-base text-customBrown font-normal">
+                  ONLY THIS WEEK
+                </p>
+                <p className="text-4xl text-customBrown font-bold">
+                  HUGE SALES
+                </p>
+              </div>
+              <div className="bg-primary flex flex-col items-center justify-center w-full h-64">
+                <p className="text-base text-white font-normal">
+                  ONLY THIS WEEK
+                </p>
+                <p className="text-4xl text-white font-bold">HUGE SALES</p>
+              </div>
+            </div>
+          </div>
+        </section>
       </header>
     </div>
   );
