430bd71cf8f90a3aaeeb060df6327114780c8b1b
kudaT <timilehinkudaisi01@gmail.com>
2025-03-14 10:33:46 -0700

    fixed issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
-<<<<<<< HEAD
 import HomeGuest from "./pages/HomeGuest";
-=======
 import HomeGuest from "./pages/private/home/HomeGuest";
-//example import, import Home from "./pages/Home";
->>>>>>> 5fb8cb3 (moved images to public folder)
+
 
 function App() {
   return (
