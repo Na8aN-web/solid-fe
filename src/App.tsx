ff10b9af788398fa0fc0019092ddbde00f7d3a80
kudaT <timilehinkudaisi01@gmail.com>
2025-03-16 04:35:54 -0700

    Refactor App.tsx setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/public/home/Home'
+import Home from "./pages/public/home/Home";
 import Layout from "./Layout";
-import HomeGuest from "./pages/HomeGuest";
 import HomeGuest from "./pages/private/home/HomeGuest";
-
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/home" element={<HomeGuest />} />
-          <Route index element={<Home />} />
-        </Route>
-      </Routes>
-    </Router>
-
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<HomeGuest />} />
+      </Route>
+    </Routes>
   );
 }
 
