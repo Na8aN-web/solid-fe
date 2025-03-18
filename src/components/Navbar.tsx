80622e2c208a7fb0f7640f1c1b5fcffae9ebe381
kudaT <timilehinkudaisi01@gmail.com>
2025-03-17 21:37:10 -0700

    Style: Make navbar responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,56 +1,38 @@
 import React from "react";
-import solidLogo from "../images/Frame 47.svg";
-import help from "../images/help.svg";
-import cart from "../images/cart.svg";
-import arrowDown from "../images/arrow-down.svg";
-import arrowDownWhite from "../images/arrow-right-white.svg";
-import arrowRight from "../images/arrowright.svg";
-import search from "../images/search.svg";
-import bodyParts from "../images/game-icons_race-car.svg";
-import electronics from "../images/iconoir_electronics-chip.svg";
-import performanceParts from "../images/performance-part.svg";
-import repairParts from "../images/repair-parts.svg";
-import wheelsTyres from "../images/wheels-tyres.svg";
-import toolsEquip from "../images/tools-equip.png";
-import steering from "../images/steering.svg";
-import filters from "../images/filter.svg";
-import coolingHeat from "../images/cooling-heat.svg";
-import airCondition from "../images/air-conditioner.svg";
-import carHeader from "../images/car-header.svg";
-// import solidLogo from "../images/Frame 47.svg";
-// import help from "../images/help.svg";
-// import cart from "../images/cart.svg";
-// import arrowDown from "../images/arrow-down.svg";
-// import arrowDownWhite from "../images/arrow-right-white.svg";
-// import arrowRight from "../images/arrowright.svg";
-// import search from "../images/search.svg";
-// import bodyParts from "../images/game-icons_race-car.svg";
-// import electronics from "../images/iconoir_electronics-chip.svg";
-// import performanceParts from "../images/performance-part.svg";
-// import repairParts from "../images/repair-parts.svg";
-// import wheelsTyres from "../images/wheels-tyres.svg";
-// import toolsEquip from "../images/tools-equip.png";
-// import steering from "../images/steering.svg";
-// import filters from "../images/filter.svg";
-// import coolingHeat from "../images/cooling-heat.svg";
-// import airCondition from "../images/air-conditioner.svg";
-
 
 const Navbar = () => {
   return (
     <div className="flex flex-col">
-      <header className="border-b">
-        <div className="flex justify-between items-center py-11 px-5 border-b">
-          <div className="flex gap-20">
-            <img src="/Frame 47.svg" alt="solid-logo" />
-            {/* dropdown */}
-            <div className="flex gap-4">
-              <div className="flex">
-                <div className="flex justify-around items-center w-44 h-12 border border-r-0 rounded-l-lg">
+      <header>
+        <div className="flex items-center pt-11 px-5 w-full relative lg:gap-20 lg:border-b lg:pb-11">
+          <div className="flex flex-col lg:flex-row gap-4 w-full lg:w-auto">
+            <div className="flex justify-between">
+              <div className="flex gap-4">
+                <img src="/hamburger.svg" alt="" className="w-6 lg:hidden" />
+                <img
+                  src="/Frame 47.svg"
+                  alt="solid-logo"
+                  className="w-32 lg:w-44"
+                />
+              </div>
+              <img src="/cart.svg" alt="" className="w-8 lg:hidden" />
+            </div>
+            {/* mobile input */}
+            <input
+              type="text"
+              className="border h-12 w-full p-2 rounded-lg lg:rounded-r-lg text-sm pl-12 lg:hidden"
+              placeholder="Search by part name of OEM number"
+            />
+          </div>
+          <div className="hidden lg:flex w-full">
+            {/* input search */}
+            <div className="flex gap-4 w-full">
+              <div className="flex w-full">
+                <div className="flex justify-around items-center w-52 gap-1 h-12 px-2 border border-r-0 rounded-l-lg">
                   <p className="text-sm">All Categories</p>
                   <img src="/arrow-down.svg" alt="arrow-down" className="w-3" />
                 </div>
-                <div className="relative">
+                <div className="relative max-w-96 w-full">
                   <img
                     src="/search.svg"
                     alt="search"
@@ -58,8 +40,8 @@ const Navbar = () => {
                   />
                   <input
                     type="text"
-                    className="border h-12 w-96 p-2 rounded-r-lg text-sm pl-12"
-                    placeholder="Search by part name of OEM number"
+                    className="border h-12 w-full p-2 rounded-r-lg text-sm pl-12"
+                    placeholder="Search by part name or OEM number"
                   />
                 </div>
               </div>
@@ -70,50 +52,55 @@ const Navbar = () => {
                 Search
               </button>
             </div>
-          </div>
-          {/* User Options */}
-          <div className="flex gap-4 items-center">
-            <div className="flex gap-1">
-              <img src="/help.svg" alt="help" className="w-8" />
-              <select defaultValue="" name="" id="" className="text-sm">
-                <option value="">Help</option>
-              </select>
+            {/* User Options */}
+            <div className="flex gap-4 items-center justify-end w-full">
+              <div className="flex gap-1 items-center">
+                <img src="/help.svg" alt="help" className="w-8" />
+                <select defaultValue="" name="" id="" className="text-sm">
+                  <option value="">Help</option>
+                </select>
+              </div>
+              <div className="flex gap-1 items-center">
+                <img src="cart.svg" alt="cart" className="w-auto" />
+                <p className="text-sm">My Cart</p>
+              </div>
+              <button
+                type="submit"
+                className="border border-primary h-12 w-20 rounded-lg bg-white text-primary text-sm font-semibold"
+              >
+                Log in
+              </button>
+              <button
+                type="submit"
+                className="border h-12 w-24 rounded-lg bg-primary text-white text-sm font-semibold"
+              >
+                Sign up
+              </button>
             </div>
-            <div className="flex gap-1">
-              <img src="cart.svg" alt="cart" className="w-8" />
-              <p className="text-sm">My Cart</p>
-            </div>
-            <button
-              type="submit"
-              className="border border-primary h-12 w-16 rounded-lg bg-white text-primary text-sm font-semibold"
-            >
-              Log in
-            </button>
-            <button
-              type="submit"
-              className="border h-12 w-20 rounded-lg bg-primary text-white text-sm font-semibold"
-            >
-              Sign up
-            </button>
           </div>
         </div>
 
         {/* Main Navigation with Sidebar Dropdown */}
         <div>
           <nav>
-            <div className="flex items-start w-full gap-10 pt-6 pb-4 pr-20">
+            <div className="flex flex-col lg:flex-row items-start w-full gap-5 pt-6 pb-4  pr-20">
               {/* Sidebar Contents */}
-              <aside className="relative">
-                <div className="">
-                  <div className="flex justify-around items-center w-72 h-14 bg-primary">
-                    <p className="text-base text-white font-normal">
-                      All Categories
+              <aside>
+                <div className="w-full">
+                  <div className="flex items-center justify-between gap-4 w-screen px-8 sm:w-72 h-14 bg-primary">
+                    <div className="flex items-center gap-4">
+                      <p className="text-base text-white font-normal">
+                        All Categories
+                      </p>
+                      <img
+                        src="/arrow-right-white.svg"
+                        alt="arrow-down"
+                        className="w-3"
+                      />
+                    </div>
+                    <p className="text-base text-white font-semibold">
+                      See All
                     </p>
-                    <img
-                      src="/arrow-right-white.svg"
-                      alt="arrow-down"
-                      className="w-3"
-                    />
                   </div>
                   {/* <ul className="flex flex-col justify-around items-start h-inherit px-11 py-3 bg-white text-gray-500 text-sm font-normal">
                     <li className="flex items-center gap-4">
@@ -161,7 +148,7 @@ const Navbar = () => {
                 </div>
               </aside>
               {/* main navigation */}
-              <ul className="flex w-full justify-between items-center pt-1 text-customGray1 text-sm font-semibold">
+              <ul className="flex w-full justify-between items-center text-customGray1 text-sm font-semibold">
                 <li className="bg-gray-100 rounded-3xl px-6 py-3">
                   All Brands
                 </li>
@@ -175,7 +162,11 @@ const Navbar = () => {
                 <li>Mazda</li>
                 <li>Audi</li>
                 <li>
-                  <img src="/arrowright.svg" alt="arrow-right" className="w-4" />
+                  <img
+                    src="/arrowright.svg"
+                    alt="arrow-right"
+                    className="w-4"
+                  />
                 </li>
               </ul>
             </div>
