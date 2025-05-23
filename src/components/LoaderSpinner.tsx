e7f81f21ec4005b2bd93923ba086743fefd03050
kudaT <timilehinkudaisi01@gmail.com>
2025-05-23 03:16:41 -0700

    feat: add loading state check and LoadingSpinner component

---
+++ src/components/LoaderSpinner.tsx
@@ -0,0 +1,9 @@
+const LoaderSpinner = () => {
+  return (
+    <div>
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-primary" />
+    </div>
+  );
+};
+
+export default LoaderSpinner;
