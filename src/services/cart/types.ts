822d1a3acb714f448a8c60aa941a22623f9a8e38
kudaT <timilehinkudaisi01@gmail.com>
2025-06-03 06:44:34 -0700

    feat(user): integrate user API with Redux state management

--- src/services/cart/types.ts
+++ src/services/cart/types.ts
@@ -1,27 +1,22 @@
 // src/services/cart/types.ts
 export interface cartProduct {
-    _id: string;
-    name: string;
-    images: string[];
-    salesPrice: number;
-    stockStatus: string;
-    quantity: number;
-    totalPrice: number;
-  }
-  
-  export interface Cart {
-    _id: string;
-    user: string;
-    products: cartProduct[];
-  }
-  
-  export interface cartState {
-    cart: Cart | null;
-    loading: boolean;
-    error: string | null;
-  }
-  
-  export interface ProductCartAction {
-    productId: string;
-  }
-  
+  _id: string;
+  name: string;
+  images: string[];
+  salesPrice: number;
+  stockStatus: string;
+  quantity: number;
+  totalPrice: number;
+}
+
+export interface Cart {
+  _id: string;
+  user: string;
+  products: cartProduct[];
+}
+
+export interface cartState {
+  cart: Cart | null;
+  loading: boolean;
+  error: string | null;
+}
