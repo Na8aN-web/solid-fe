d791e39e8fd406eedf470e7b27178d04e630be3a
kudaT <timilehinkudaisi01@gmail.com>
2025-07-04 02:20:37 -0700

    feat: add admin interface for managing products categories, vehicles and brands

--- src/pages/admin/productCategory/ProductCat.tsx
+++ src/pages/admin/productCategory/ProductCat.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Search, Plus, ChevronDown, ArrowUpDown } from "lucide-react";
 import ProductIcon from "../../../assets/productIcon.svg";
 import carTyre from "../../../assets/tyres.svg";
@@ -12,53 +12,267 @@ interface Product {
   createdDate: string;
 }
 
-const Products: React.FC = () => {
-  const products: Product[] = [
-    {
-      id: "1",
-      product: "Michellene Tyre",
-      image: carTyre,
-      createdDate: "25th July, 2024",
-    },
-    {
-      id: "2",
-      product: "Michellene Tyre",
-      image: carTyre,
-      createdDate: "25th July, 2024",
-    },
-    {
-      id: "3",
-      product: "Michellene Tyre",
-      image: carTyre,
-      createdDate: "25th July, 2024",
-    },
-    {
-      id: "4",
-      product: "Michellene Tyre",
-      image: carTyre,
-      createdDate: "25th July, 2024",
-    },
-  ];
-  const getStatusColor = (status: string) => {
-    switch (status) {
-      case "Active":
-        return "bg-[#E1F1E0] text-[#15B70D]";
-      case "Inactive":
-        return "bg-[#F3F3F3] text-[#919191]";
-      default:
-        return "bg-gray-100 text-gray-800";
-    }
-  };
+const products: Product[] = [
+  {
+    id: "1",
+    product: "Michellene Tyre",
+    image: carTyre,
+    createdDate: "25th July, 2024",
+  },
+  {
+    id: "2",
+    product: "Michellene Tyre",
+    image: carTyre,
+    createdDate: "25th July, 2024",
+  },
+  {
+    id: "3",
+    product: "Michellene Tyre",
+    image: carTyre,
+    createdDate: "25th July, 2024",
+  },
+  {
+    id: "4",
+    product: "Michellene Tyre",
+    image: carTyre,
+    createdDate: "25th July, 2024",
+  },
+];
 
-  const handleEdit = (id: string) => {
-    console.log("Editing product with ID:", id);
-    // Navigate to edit form or open modal
-  };
+const productsBrand: Product[] = [
+  {
+    id: "1",
+    product: "Michellene brand",
+    image: carTyre,
+    createdDate: "25th July, 2024",
+  },
+  {
+    id: "2",
+    product: "Michellene brand",
+    image: carTyre,
+    createdDate: "25th July, 2024",
+  },
+  {
+    id: "3",
+    product: "Michellene brand",
+    image: carTyre,
+    createdDate: "25th July, 2024",
+  },
+  {
+    id: "4",
+    product: "Michellene Tyre",
+    image: carTyre,
+    createdDate: "25th July, 2024",
+  },
+];
 
-  const handleDelete = (id: string) => {
-    console.log("Deleting product with ID:", id);
-    // Show confirm dialog or call API
-  };
+const getStatusColor = (status: string) => {
+  switch (status) {
+    case "Active":
+      return "bg-[#E1F1E0] text-[#15B70D]";
+    case "Inactive":
+      return "bg-[#F3F3F3] text-[#919191]";
+    default:
+      return "bg-gray-100 text-gray-800";
+  }
+};
+
+const handleEdit = (id: string) => {
+  console.log("Editing product with ID:", id);
+  // Navigate to edit form or open modal
+};
+
+const handleDelete = (id: string) => {
+  console.log("Deleting product with ID:", id);
+  // Show confirm dialog or call API
+};
+
+// product categories table
+const Categories = () => {
+  return (
+    <div className="overflow-x-auto">
+      <table className="w-full">
+        <thead>
+          <tr className="text-left text-sm bg-[#F8F8F8] text-gray-600 border-b">
+            <th className="p-4">
+              <div className="w-5 h-5 border border-[#D9D9D9] bg-white rounded-[4px]"></div>
+            </th>
+            <th className="p-4">Name</th>
+            <th className="p-4">Created Date</th>
+            <th className="p-4">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr
+              key={product.id}
+              className="border-b text-[#5E5E5E] last:border-b-0"
+            >
+              <td className="p-4">
+                <div className="w-5 h-5 border border-[#D9D9D9] bg-white rounded-[4px]"></div>
+              </td>
+              <td className="p-4 text-sm flex items-center justify-start gap-4">
+                <div className="w-12 h-12 bg-[#FAF9F9] rounded-lg flex items-center justify-center">
+                  <img
+                    src={product.image}
+                    alt={product.product}
+                    className="w-[26px]"
+                  />
+                </div>
+                {product.product}
+              </td>
+              <td className="p-4 text-sm">{product.createdDate}</td>
+
+              <td className="p-4">
+                <div className="flex items-center gap-6">
+                  <button
+                    onClick={() => handleEdit(product.id)}
+                    className="w-[55px] h-[30px] bg-primary rounded-[6px] text-white flex items-center justify-center text-xs font-semibold gap-1"
+                  >
+                    <img src={edit} alt="" />
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => handleDelete(product.id)}
+                    className="border border-[#E3E6EA] bg-white p-2 rounded-[8px]"
+                  >
+                    <img src={deLete} alt="" />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+// brands table
+const Brands = () => {
+  return (
+    <div className="overflow-x-auto">
+      <table className="w-full">
+        <thead>
+          <tr className="text-left text-sm bg-[#F8F8F8] text-gray-600 border-b">
+            <th className="p-4">
+              <div className="w-5 h-5 border border-[#D9D9D9] bg-white rounded-[4px]"></div>
+            </th>
+            <th className="p-4">Name</th>
+            <th className="p-4">Created Date</th>
+            <th className="p-4">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {productsBrand.map((product) => (
+            <tr
+              key={product.id}
+              className="border-b text-[#5E5E5E] last:border-b-0"
+            >
+              <td className="p-4">
+                <div className="w-5 h-5 border border-[#D9D9D9] bg-white rounded-[4px]"></div>
+              </td>
+              <td className="p-4 text-sm flex items-center justify-start gap-4">
+                <div className="w-12 h-12 bg-[#FAF9F9] rounded-lg flex items-center justify-center">
+                  <img
+                    src={product.image}
+                    alt={product.product}
+                    className="w-[26px]"
+                  />
+                </div>
+                {product.product}
+              </td>
+              <td className="p-4 text-sm">{product.createdDate}</td>
+
+              <td className="p-4">
+                <div className="flex items-center gap-6">
+                  <button
+                    onClick={() => handleEdit(product.id)}
+                    className="w-[55px] h-[30px] bg-primary rounded-[6px] text-white flex items-center justify-center text-xs font-semibold gap-1"
+                  >
+                    <img src={edit} alt="" />
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => handleDelete(product.id)}
+                    className="border border-[#E3E6EA] bg-white p-2 rounded-[8px]"
+                  >
+                    <img src={deLete} alt="" />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+// vehicles table
+const Vehicles = () => {
+  return (
+    <div className="overflow-x-auto">
+      <table className="w-full">
+        <thead>
+          <tr className="text-left text-sm bg-[#F8F8F8] text-gray-600 border-b">
+            <th className="p-4">
+              <div className="w-5 h-5 border border-[#D9D9D9] bg-white rounded-[4px]"></div>
+            </th>
+            <th className="p-4">Name</th>
+            <th className="p-4">Created Date</th>
+            <th className="p-4">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr
+              key={product.id}
+              className="border-b text-[#5E5E5E] last:border-b-0"
+            >
+              <td className="p-4">
+                <div className="w-5 h-5 border border-[#D9D9D9] bg-white rounded-[4px]"></div>
+              </td>
+              <td className="p-4 text-sm flex items-center justify-start gap-4">
+                <div className="w-12 h-12 bg-[#FAF9F9] rounded-lg flex items-center justify-center">
+                  <img
+                    src={product.image}
+                    alt={product.product}
+                    className="w-[26px]"
+                  />
+                </div>
+                {product.product}
+              </td>
+              <td className="p-4 text-sm">{product.createdDate}</td>
+
+              <td className="p-4">
+                <div className="flex items-center gap-6">
+                  <button
+                    onClick={() => handleEdit(product.id)}
+                    className="w-[55px] h-[30px] bg-primary rounded-[6px] text-white flex items-center justify-center text-xs font-semibold gap-1"
+                  >
+                    <img src={edit} alt="" />
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => handleDelete(product.id)}
+                    className="border border-[#E3E6EA] bg-white p-2 rounded-[8px]"
+                  >
+                    <img src={deLete} alt="" />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const ProductCat: React.FC = () => {
+  const [activeTab, setActiveTab] = useState<
+    "categories" | "brands" | "vehicles"
+  >("categories");
+
   return (
     <div className="p-10">
       <section className="flex items-center justify-between mb-8">
@@ -82,9 +296,36 @@ const Products: React.FC = () => {
       </section>
       <section className="flex items-center justify-between mb-8">
         <div className="flex items-center gap-6">
-          <h2>Product Categories</h2>
-          <h2>Brand Name</h2>
-          <h2>Vehicle Types</h2>
+          <h2
+            onClick={() => setActiveTab("categories")}
+            className={`cursor-pointer ${
+              activeTab === "categories"
+                ? "text-sm bg-[#F3F3F3] p-2 rounded-[6px]"
+                : "text-sm text-customBrown"
+            }`}
+          >
+            Product Categories
+          </h2>
+          <h2
+            onClick={() => setActiveTab("brands")}
+            className={`cursor-pointer ${
+              activeTab === "brands"
+                ? "text-sm bg-[#F3F3F3] p-2 rounded-[6px]"
+                : "text-sm text-customBrown"
+            }`}
+          >
+            Brand Name
+          </h2>
+          <h2
+            onClick={() => setActiveTab("vehicles")}
+            className={`cursor-pointer ${
+              activeTab === "vehicles"
+                ? "text-sm bg-[#F3F3F3] p-2 rounded-[6px]"
+                : "text-sm text-customBrown"
+            }`}
+          >
+            Vehicle Types
+          </h2>
         </div>
         <div className="flex items-center gap-2">
           <div className="w-[75px] h-[52px] bg-[#F8F8F8] flex items-center justify-center gap-1 rounded-[6px]">
@@ -93,63 +334,11 @@ const Products: React.FC = () => {
           </div>
         </div>
       </section>
-      <div className="overflow-x-auto">
-        <table className="w-full">
-          <thead>
-            <tr className="text-left text-sm bg-[#F8F8F8] text-gray-600 border-b">
-              <th className="p-4">
-                <div className="w-5 h-5 border border-[#D9D9D9] bg-white rounded-[4px]"></div>
-              </th>
-              <th className="p-4">Name</th>
-              <th className="p-4">Created Date</th>
-              <th className="p-4">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr
-                key={product.id}
-                className="border-b text-[#5E5E5E] last:border-b-0"
-              >
-                <td className="p-4">
-                  <div className="w-5 h-5 border border-[#D9D9D9] bg-white rounded-[4px]"></div>
-                </td>
-                <td className="p-4 text-sm flex items-center justify-start gap-4">
-                  <div className="w-12 h-12 bg-[#FAF9F9] rounded-lg flex items-center justify-center">
-                    <img
-                      src={product.image}
-                      alt={product.product}
-                      className="w-[26px]"
-                    />
-                  </div>
-                  {product.product}
-                </td>
-                <td className="p-4 text-sm">{product.createdDate}</td>
-
-                <td className="p-4">
-                  <div className="flex items-center gap-6">
-                    <button
-                      onClick={() => handleEdit(product.id)}
-                      className="w-[55px] h-[30px] bg-primary rounded-[6px] text-white flex items-center justify-center text-xs font-semibold gap-1"
-                    >
-                      <img src={edit} alt="" />
-                      Edit
-                    </button>
-                    <button
-                      onClick={() => handleDelete(product.id)}
-                      className="border border-[#E3E6EA] bg-white p-2 rounded-[8px]"
-                    >
-                      <img src={deLete} alt="" />
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {activeTab === "categories" && <Categories />}
+      {activeTab === "brands" && <Brands />}
+      {activeTab === "vehicles" && <Brands />}
     </div>
   );
 };
 
-export default Products;
+export default ProductCat;
